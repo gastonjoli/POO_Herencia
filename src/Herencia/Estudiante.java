47a21b830a0833f8aadae5efc93d4298a731341c
Falso <falsee63@gmail.com>
2021-10-10 00:40:10 -0300

    POO Herencia

--- src/Herencia/Estudiante.java
+++ src/Herencia/Estudiante.java
@@ -1,10 +1,9 @@
 package Herencia;
 
 /*
-PASO 2: INDICARLE A JAVA QUE ESTUDIANTE ES UNA
+PASO 2: Debemos INDICARLE A JAVA QUE ESTUDIANTE ES UNA
 CLASE HIJA DE PERSONA con la palabra EXTENDS.
 de la siguiente forma:
-
  */
 public class Estudiante extends Persona{
     private int codigoEstudiante;
@@ -14,12 +13,29 @@ public class Estudiante extends Persona{
 
     public Estudiante(String nombre,String apellido,int edad,int codigoEstudiante,float notaFinal){
         super(nombre,apellido,edad); // Le decimos a java con SUPER que nom apell y edad ya estan inicializados.
+
         // lo que si falta inicializar son los atributos extras que tiene estudiante:
+
         this.codigoEstudiante = codigoEstudiante;
         this.notaFinal = notaFinal;
+     }
+     /*
+     Cuando quiero llamar a mi atributos nombre, apellido y edad
+     veremos que no se puede porque no pertenece a esta clase por
+     lo que tenemos que utilizarlos métodos getter de la
+     clase persona linea 30 - 31 -32.
+     Una vez terminados de poner los datos de la clase persona
+     poner los atributos que solo pertenecen a estudiante sin
+     usar los getter porque como son de la misma clase podemos
+     ponerlo directo LINEAS 33 y 34.
+      */
+     public void mostrardatos(){
+         System.out.println("Nombre del estudiante: "+getNombre()+
+                 "\nApellido: "+getApellido()+
+                 "\nEdad: "+getEdad()+
+                 "\nCodigo Estudiante:"+codigoEstudiante+
+                 "\nNota Final: "+notaFinal);
 
-    }
-
-
+     }
 
 }
--- src/Herencia/Main.java
+++ src/Herencia/Main.java
@@ -1,4 +1,16 @@
 package Herencia;
-
+//PASO 3:
+/*
+lo que vamos a hacer aca es crear un objeto de la clase Estudiante y vamos
+a poner un nuevo estudiante y dentro de los parentesis inicializaremos todos
+los datos lo que sta dentro de los parentesis.
+ */
 public class Main {
+    public static void main(String[] args) {
+        Estudiante estudiante = new Estudiante("Gaston","Jolinont",34,456,90.6f);
+
+        estudiante.mostrardatos();
+
+
+    }
 }
